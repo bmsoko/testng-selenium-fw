8b08a3191a18e425620ebbf8cc95124205a222bb
Bruno Soko <sokobruno@gmail.com>
2020-06-17 23:18:58 -0300

    Adding POM for homepage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,25 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomePage {
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
     // Mappings
+    By searchBox  = By.id("search-key");
+    By searchBtn  = By.className("search-button");
+    By closePopUp = By.cssSelector("[class=\"close-layer\"]");
+    By iFrame     = By.id("localstorage-proxy-ifr-alibabadotcom2");
+
 
 
 
     //Actions
+    public void searchForItem (String itemToSearch) {
+        clear(searchBox);
+        type(itemToSearch, searchBox);
+        click(searchBtn);
+    }
 }
